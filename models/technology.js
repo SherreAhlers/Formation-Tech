fc6cca27537206eb9e7df09487241c1ddcf61c5c
SherreAhlers <sherre.ahlers@gmail.com>
2020-10-17 18:26:58 -0500

    Deploy 3

--- models/technology.js
+++ models/technology.js
@@ -1,20 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// const commentSchema = new Schema({
-//   comments: String,
-//   owner: { type: Schema.Types.ObjectId, ref: 'User'},
-// }, {
-//   timestamps: true
-// });
-
 const technologySchema = new Schema({
   name: {type: String, required: true},
   description: {type: String, required: true},
   owner: {type: Schema.Types.ObjectId, ref:'User'},
   image: {type: String, required: false},
   infoURL: {type: String, required: false},
-  // comments: [commentSchema]
+ 
 }, {
   timestamps: true
 });
