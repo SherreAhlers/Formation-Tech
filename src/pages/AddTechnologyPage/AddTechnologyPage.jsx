4becddf9be8577d724dec0d5544597d081fdd2d0
SherreAhlers <sherre.ahlers@gmail.com>
2020-12-23 11:42:42 -0600

    Update Styling

--- src/pages/AddTechnologyPage/AddTechnologyPage.jsx
+++ src/pages/AddTechnologyPage/AddTechnologyPage.jsx
@@ -1,60 +1,67 @@
-import React, {Component} from 'react';
-import '../AddTechnologyPage/AddTechnologyPage.css';
-
+import React, { Component } from "react";
+import "../AddTechnologyPage/AddTechnologyPage.css";
 
 class AddTechnologyPage extends Component {
   state = {
     invalidForm: true,
     formData: {
-      name: '',
-      description: '',
+      name: "",
+      description: "",
       // image: '', icebox item
-      infoURL: ''
-    }
+      infoURL: "",
+    },
   };
 
   formRef = React.createRef();
 
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault();
     this.props.handleAddTechnology(this.state.formData);
   };
 
-  handleChange = e => {
-    const formData = {...this.state.formData, [e.target.name]: e.target.value};
+  handleChange = (e) => {
+    const formData = {
+      ...this.state.formData,
+      [e.target.name]: e.target.value,
+    };
     this.setState({
       formData,
-      invalidForm: !this.formRef.current.checkValidity()
+      invalidForm: !this.formRef.current.checkValidity(),
     });
   };
 
   render() {
     return (
       <>
-      <h1>Add Technology</h1>
-      <div className="add" id="add-form">
-        <form ref={this.formRef} autoComplete="off" onSubmit={this.handleSubmit}>
-          <div className="form-group" id="add-name">
-            <label>Technology Name (required)</label>
-            <input
-              className="form-control"
-              name="name"
-              value={this.state.formData.name}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="form-group" id="add-description">
-            <label>Technology Description (required)</label>
-            <input
-              className="form-control"
-              name="description"
-              value={this.state.formData.description}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          {/* <div className="form-group" id="add-image">
+        <div className="card">
+          <h1 className="add">Add Technology</h1>
+          <div className="add2" id="add-form">
+            <form
+              ref={this.formRef}
+              autoComplete="off"
+              onSubmit={this.handleSubmit}
+            >
+              <div className="form-group" id="add-name">
+                <label>Technology Name (required)</label>
+                <input
+                  className="form-control"
+                  name="name"
+                  value={this.state.formData.name}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <div className="form-group" id="add-description">
+                <label>Technology Description (required)</label>
+                <input
+                  className="form-control"
+                  name="description"
+                  value={this.state.formData.description}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              {/* <div className="form-group" id="add-image">
             <label>Image</label>
             <input
               className="form-control"
@@ -62,25 +69,26 @@ class AddTechnologyPage extends Component {
               value={this.state.formData.image}
               onChange={this.handleChange}
             />
-          </div> */} {/* icebox item */}
-          <div className="form-group" id="add-url">
-            <label>Resource URL</label>
-            <input
-              className="form-control"
-              name="infoURL"
-              value={this.state.formData.infoURL}
-              onChange={this.handleChange}
-            />
+          </div>  icebox item */}
+              <div className="form-group" id="add-url">
+                <label>Resource URL</label>
+                <input
+                  className="form-control"
+                  name="infoURL"
+                  value={this.state.formData.infoURL}
+                  onChange={this.handleChange}
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn"
+                id="add-button"
+                disabled={this.state.invalidForm}
+              >
+                ADD Technology
+              </button>
+            </form>
           </div>
-          <button
-            type="submit"
-            className="btn"
-            id="add-button"
-            disabled={this.state.invalidForm}
-          >
-            ADD Technology
-          </button>
-        </form>
         </div>
       </>
     );
--- src/pages/EditTechnologyPage/EditTechnologyPage.jsx
+++ src/pages/EditTechnologyPage/EditTechnologyPage.jsx
@@ -1,57 +1,63 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import '../EditTechnologyPage/EditTechnologyPage.css';
-
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../EditTechnologyPage/EditTechnologyPage.css";
 
 class EditTechnologyPage extends Component {
   state = {
     invalidForm: false,
-    formData: this.props.location.state.technology
+    formData: this.props.location.state.technology,
   };
 
   formRef = React.createRef();
 
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault();
     this.props.handleUpdateTechnology(this.state.formData);
   };
 
-  handleChange = e => {
-    const formData = {...this.state.formData, [e.target.name]: e.target.value};
+  handleChange = (e) => {
+    const formData = {
+      ...this.state.formData,
+      [e.target.name]: e.target.value,
+    };
     this.setState({
       formData,
-      invalidForm: !this.formRef.current.checkValidity()
+      invalidForm: !this.formRef.current.checkValidity(),
     });
   };
 
   render() {
     return (
       <>
-      <h1>Edit Technology</h1>
-      <div className="edit" id="edit-form">
-        <form ref={this.formRef} autoComplete="off" onSubmit={this.handleSubmit}>
-          <div className="form-group" id="technology">
-            <label>Technology Name (required)</label>
-            <input
-              className="form-control"
-              name="name"
-              value={this.state.formData.name}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="form-group" id="description">
-            <label>Description (required)</label>
-            <input
-              className="form-control"
-              name="description"
-              value={this.state.formData.description}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          {/* <div className="form-group" id="image">
+        <div class="card">
+          <h1 class="edit-tech">Edit Technology</h1>
+          <div className="edit" id="edit-form">
+            <form
+              ref={this.formRef}
+              autoComplete="off"
+              onSubmit={this.handleSubmit}
+            >
+              <div className="form-group" id="technology">
+                <label>Technology Name (required)</label>
+                <input
+                  className="form-control"
+                  name="name"
+                  value={this.state.formData.name}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <div className="form-group" id="description">
+                <label>Description (required)</label>
+                <input
+                  className="form-control"
+                  name="description"
+                  value={this.state.formData.description}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              {/* <div className="form-group" id="image">
             <label>Image</label>
             <input
               className="form-control"
@@ -60,27 +66,30 @@ class EditTechnologyPage extends Component {
               onChange={this.handleChange}
               required
             />
-          </div> */}  {/* icebox item */}
-          <div className="form-group" id="infoURL">
-            <label>Information URL</label>
-            <input
-              className="form-control"
-              name="infoURL"
-              value={this.state.formData.infoURL}
-              onChange={this.handleChange}
-              required
-            />
+          </div> */}{" "}
+              {/* icebox item */}
+              <div className="form-group" id="infoURL">
+                <label>Information URL</label>
+                <input
+                  className="form-control"
+                  name="infoURL"
+                  value={this.state.formData.infoURL}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-xs"
+                id="save-button"
+                disabled={this.state.invalidForm}
+              >
+                SAVE TECHNOLOGY
+              </button>
+              &nbsp;&nbsp;
+              <Link to="/">CANCEL</Link>
+            </form>
           </div>
-          <button
-            type="submit"
-            className="btn btn-xs"
-            id="save-button"
-            disabled={this.state.invalidForm}
-          >
-            SAVE TECHNOLOGY
-          </button>&nbsp;&nbsp;
-          <Link to='/'>CANCEL</Link>
-        </form>
         </div>
       </>
     );
